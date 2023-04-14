059bb4d5f46f657bb7047c417351ef0ead6ec089
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-04-14 12:57:34 +0200

    pokemon name list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    
-    interface Pokemon{
+    import "../app.css"
+    import { onMount } from 'svelte'
+
+    interface Pokemon {
         name: string
     }
 
+    let pokemonList: Pokemon[] = []
 
-  let pokemonList: Pokemon[] = [];
+    async function fetchPokemon(): Promise<void> {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+        const data = await response.json()
+        pokemonList = data.results
+    }
 
-async function fetchPokemon() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=12')
-    const data = await response.json()
-    pokemonList = data.results
-}
-
-
-onMount(() => {
-    fetchPokemon();
-  });
+    onMount((): void => {
+        fetchPokemon()
+    })
 </script>
 
-<h1>Pokemon List</h1>
-<ul>
-  {#each pokemonList as pokemon}
-    <li>{pokemon.name}</li>
-  {/each}
-</ul>
+    <div class="mx-auto max-w-7xl">
+        <h1 class="mb-4 text-3xl font-bold text-center">Pokemon List</h1>
+        <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto">
+          {#each pokemonList as pokemon}
+                <li class="rounded-lg bg-gray-400 p-4 text-center">{pokemon.name}</li>
+                
+            {/each}
+        </ul>
+    </div>
