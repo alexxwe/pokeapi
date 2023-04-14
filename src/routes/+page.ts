ecfcefd2b3e7607f2431a824d73ac1b821fca9b0
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-04-15 01:58:50 +0200

    load function with pokemonData
    +page.ts
    
    https://kit.svelte.dev/docs/load

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,13 +4,22 @@ import type { PokemonListDto } from '../lib/types/pokemonList.dto.js'
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }): Promise<{ pokemons: Array<PokemonDto> }> {
     try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=450&limit=50')
         const data: PokemonListDto = await response.json()
 
         const pokemons: Array<PokemonDto> = []
-        //----------------------------
-        //----------------------------
-        
+        for (const result of data.results) {
+            const pokemonResponse = await fetch(result.url)
+            const pokemonData = await pokemonResponse.json()
+
+            pokemons.push({
+                forms: pokemonData.forms,
+                sprites: pokemonData.sprites,
+                id: pokemonData.id,
+                types: pokemonData.types,
+                moves: pokemonData.moves,
+            })
+        }
 
         return {
             pokemons,
