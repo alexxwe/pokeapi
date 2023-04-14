dad181136608d51be2afed670ecc45661075b653
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-04-14 02:00:40 +0200

    list with names

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    
+    interface Pokemon{
+        name: string
+    }
+
+
+  let pokemonList: Pokemon[] = [];
+
+async function fetchPokemon() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=12')
+    const data = await response.json()
+    pokemonList = data.results
+}
+
+
+onMount(() => {
+    fetchPokemon();
+  });
+</script>
+
+<h1>Pokemon List</h1>
+<ul>
+  {#each pokemonList as pokemon}
+    <li>{pokemon.name}</li>
+  {/each}
+</ul>
