5492a4a56977e6205d3b6efd44fd0c96cc32f6ee
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-04-15 01:56:22 +0200

    interface

--- src/lib/types/pokemon.dto.ts
+++ src/lib/types/pokemon.dto.ts
@@ -5,4 +5,15 @@ export interface PokemonDto {
     sprites: {
         front_default: string
     }
+    id: number
+    types: Array<{
+        type: {
+            name: string
+        }
+    }> 
+    moves: Array<{
+        move: {
+            name: string
+        }
+    }>
 }
