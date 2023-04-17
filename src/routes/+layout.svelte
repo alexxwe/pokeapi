19d6b63a51d57b53584d77f0410c2982ae69f99f
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-04-17 13:33:50 +0200

    navigating and app.css to +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,21 @@
-<header>
-    nav
-</header>
+<script lang="ts">
+    import '../app.css'
+    import { navigating } from '$app/stores'
+</script>
+
+<header>nav</header>
 
 <main class="bg-zinc-900 text-white">
-    <slot />
+    {#if $navigating}
+        {#if $navigating?.to?.url.pathname.includes('pokemons')}
+            <div class="flex items-center justify-center">
+                <i class="bi bi-arrow-clockwise my-12 animate-spin text-6xl" />
+            </div>
+            <h1 class="text-center text-2xl">Getting data for: <br /> {$navigating?.to?.url.pathname}</h1>
+        {/if}
+    {:else}
+        <slot />
+    {/if}
 </main>
 
-<footer>
-    footer
-</footer>
+<footer>footer</footer>
