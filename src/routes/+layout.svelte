1c75866db94c9a700e7b8e15e3d3f26789750a1a
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-04-17 13:59:27 +0200

    layout fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
     import '../app.css'
-    import { navigating } from '$app/stores'
 </script>
 
-<header>nav</header>
+<header>
+    nav
+</header>
 
 <main class="bg-zinc-900 text-white">
-    {#if $navigating}
-        {#if $navigating?.to?.url.pathname.includes('pokemons')}
-            <div class="flex items-center justify-center">
-                <i class="bi bi-arrow-clockwise my-12 animate-spin text-6xl" />
-            </div>
-            <h1 class="text-center text-2xl">Getting data for: <br /> {$navigating?.to?.url.pathname}</h1>
-        {/if}
-    {:else}
-        <slot />
-    {/if}
+    <slot />
 </main>
 
-<footer>footer</footer>
+<footer>
+    footer
+</footer>
