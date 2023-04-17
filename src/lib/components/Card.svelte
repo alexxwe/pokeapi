0760bf7ac9e3e33fdd43b1b5f299e68799e089ed
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-04-17 14:00:08 +0200

    card file to a component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import type { PokemonDto } from '$lib/types/pokemon.dto'
+    import { bgColors, borderColors, typesColors } from '$lib/utils/colorType'
+
+    export let pokemon: PokemonDto
+    
+</script>
+
+<li
+    class="flex flex-col items-center rounded-lg {bgColors[pokemon.types[0].type.name]} {borderColors[
+        pokemon.types[0].type.name
+    ]} animate__animated animate__fadeInDown p-4 text-center"
+>
+    <h2 class="mb-2 text-lg font-bold">{pokemon.forms[0].name} #{pokemon.id}</h2>
+    {#each pokemon.types as types}
+        <span class="mb-0.5 rounded-full border px-2 shadow {typesColors[types.type.name]}">{types.type.name}</span>
+    {/each}
+    <img src={pokemon.sprites.front_default} alt={pokemon.forms[0].name} />
+    <ul class="grid grid-cols-2 gap-2 text-center">
+        {#each pokemon.moves.slice(0, 4) as moves}
+            <!-- <li class="rounded-lg px-2 py-2 text-black bg-zinc-300">{pokemon.moves[Math.floor(Math.random() * pokemon.moves.length)].move.name}</li> -->
+        {/each}
+    </ul>
+</li>
--- src/routes/pokemons/[slug]/+page.svelte
+++ src/routes/pokemons/[slug]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { bgColors, borderColors, typesColors } from '$lib/utils/colorType'
+    import Card from '$lib/components/Card.svelte'
     import type { PokemonDto } from '$lib/types/pokemon.dto'
     import { generations } from '$lib/utils/gens'
 
@@ -19,18 +19,7 @@
     </div>
     <ul class="mx-auto grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
         {#each data.pokemons as pokemon}
-            <li class="flex flex-col items-center rounded-lg {bgColors[pokemon.types[0].type.name]} {borderColors[pokemon.types[0].type.name]} p-4 text-center">
-                <h2 class="mb-2 text-lg font-bold">{pokemon.forms[0].name} #{pokemon.id}</h2>
-                {#each pokemon.types as types}
-                    <span class="mb-0.5 rounded-full border px-2 shadow {typesColors[types.type.name]}">{types.type.name}</span>
-                {/each}
-                <img src={pokemon.sprites.front_default} alt={pokemon.forms[0].name} />
-                <ul class="grid grid-cols-2 gap-2 text-center">
-                    {#each pokemon.moves.slice(0, 4) as moves}
-                        <!-- <li class="rounded-lg px-2 py-2 text-black bg-zinc-300">{pokemon.moves[Math.floor(Math.random() * pokemon.moves.length)].move.name}</li> -->
-                    {/each}
-                </ul>
-            </li>
+            <Card {pokemon} />
         {/each}
     </ul>
 </div>
