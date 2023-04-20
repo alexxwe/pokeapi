709a9561b66277fee75b2dce76e6d0b57fe9abc5
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-04-20 11:44:43 +0200

    Sprites and pokedex button

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -16,7 +16,9 @@ href="/pokemon/{pokemon.id}"
     {#each pokemon.types as types}
         <span class="mb-0.5 rounded-full border px-2 shadow {typesColors[types.type.name]}">{types.type.name}</span>
     {/each}
-    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+    <img src={pokemon.sprites.versions['generation-v']['black-white'].animated.front_default ?? pokemon.sprites.front_default ?? pokemon.sprites.other['official-artwork'].front_default}
+ 
+    alt={pokemon.name} />
     <ul class="grid grid-cols-2 gap-2 text-center">
         {#each pokemon.moves.slice(0, 4) as moves}
             <!-- <li class="rounded-lg px-2 py-2 text-black bg-zinc-300">{pokemon.moves[Math.floor(Math.random() * pokemon.moves.length)].move.name}</li> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<div>  ____________ </div>
-<a href="/generations/1">|·POKEDEX·|</a>
-<div> ¯¯¯¯¯¯¯¯¯¯¯¯ </div>
+<div class="my-8">
+<a href="/generations/1" class=" m-2 p-2 rounded border-2 bg-red-600 hover:shadow-red-600 shadow-lg border-red-600" >POKEDEX</a>
+</div>
